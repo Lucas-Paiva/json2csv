6cbc296caa7a2435db9b9e9bfeb820c00e5fe4d2
Lucas Paiva <67245501+Lucas-Paiva@users.noreply.github.com>
2021-06-13 20:26:51 -0300

    1.1_beta
    The nested & flattered JSON issue was been fixed.
    The data preview windows must be improved.

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # json2csv
-Simple GUI aplication to convert JSON to CSV builded with PySimpleGUI.
+Simple GUI aplication to convert JSON to CSV builded with PySimpleGUI and Pandas.
+Maybe this can be useful, and should be increased soon with new features.
--- json2csv.py
+++ json2csv.py
@@ -1,16 +1,5 @@
-# JSON converter to CSV
-
-# Issues:
-# 1 - Choose the name of the file - OK
-# 2 - Choose the destiny of the file
-# 3 - Choose the Encoding of the CSV
-# 4 - Choose the Delimiter - OK
-# 5 - Fix flattered & nested list issues
-# 6 - Freeze all with cxfreeze
-# 7 - Add 'Preview' tool
-# 8 - Align all with columns
-
 import json as jsn
+from PySimpleGUI.PySimpleGUI import popup_error
 import pandas as pd
 import PySimpleGUI as sg
 import os
@@ -32,8 +21,6 @@ layout = [
     [sg.Menu(menu_def)],
     [sg.Image(src)],
     [
-        sg.Text("JSON type:"),
-        sg.Combo(["Flattened", "Nested"], enable_events=True, key="combo"),
         sg.Text("Delimiter: "),
         sg.Input(default_text=",", size=(3, 1), key="-DEL-"),
         sg.Text("Encoding: "),
@@ -42,29 +29,38 @@ layout = [
     [
         sg.Text("Choosen file source: "),
         sg.Input(),
-        sg.FileBrowse(key="-IN-"),
+        sg.FileBrowse(key="-IN-", file_types=(("JSON Files", "*.json"),)),
     ],
     [sg.Text("Converted File name: "), sg.Input(key="-NAME-"), sg.Text(".csv")],
-    [sg.Text("Data preview"), sg.Output(size=(45, 5), key="_OUTPUT_")],
+    [sg.Text("Data preview"), sg.Multiline(size=(45, 5), key="_OUTPUT_")],
     [sg.Button("Preview"), sg.Button("Export"), sg.Button("Cancel")],
 ]
 
-#  Building Window
-window = sg.Window(
-    "JSON2CSV Converter", layout, icon=icon_src, size=(600, 300), resizable=True
-)
+#  Building Main Window
+window = sg.Window("JSON2CSV Converter", layout, icon=icon_src, size=(600, 300))
 
 
 def countColumns(file):
     json = pd.read_json(file)
     nColumns = len(json.columns)
-    print(nColumns)
     return nColumns
 
 
 def previewData(file):
+    if countColumns(file) > 2:
+        json = pd.read_json(file)
+    else:
+        with open(file, "r") as f:
+            file_name = ".\\" + file_rename + ".csv"
+            data = jsn.loads(f.read())
+            json = pd.json_normalize(data, getNestedColumn(file_source))
+    return json
+
+
+def getNestedColumn(file):
     json = pd.read_json(file)
-    return json.head()
+    columns = list(json.columns)
+    return columns[1]
 
 
 def convertToCSV(file):
@@ -75,9 +71,21 @@ def convertToCSV(file):
 
 def nestedjson2csv(file, rec_path):
     with open(file, "r") as f:
+        file_name = ".\\" + file_rename + ".csv"
         json = jsn.loads(f.read())
         df = pd.json_normalize(json, record_path=[rec_path])
-        csv = json.to_csv(".\converted.csv", header=True)
+        csv = df.to_csv(file_name, header=True, sep=csv_delimiter)
+
+
+def previewWindow(data):  # build another window to display the data preview
+    layout = [[sg.Text(data)]]
+    window = sg.Window("Data Preview", layout, size=(600, 300))
+    event, values = window.read()
+    while True:
+        if event == "Exit" or event == sg.WIN_CLOSED:
+            break
+
+    window.close()
 
 
 while True:
@@ -91,16 +99,18 @@ while True:
         sg.popup("Version 1.0")
 
     if event == "Export":
-        n = countColumns(values["-IN-"])
+        n = countColumns(file_source)
         print(file_source)
 
         if n > 2:
-            convertToCSV(values["-IN-"])
+            convertToCSV(file_source)
+        elif n <= 2:
+            nestedjson2csv(file_source, getNestedColumn(file_source))
+
         sg.popup("JSON sucessfully converted to CSV")
 
     if event == "Preview":
-        print(previewData(values["-IN-"]))
-        # window["_OUTPUT_"].update(data)
+        previewWindow(previewData(file_source))
 
     if event == sg.WIN_CLOSED or event == "Exit" or event == "Cancel":
         break
